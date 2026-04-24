efe98d220b9334932078f7df991289757aa0382b
kayalraju <kayalraju003@gmail.com>
2026-04-24 09:49:23 +0530

    basic php

---
+++ datatype/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$user="Pritam";
+$age=21;
+$salary=1000.00;
+$array=["pritam","raju","pallav","sagnik","trisha"];
+
+
+
+echo  "<h1>user name is $user </h1>";
+echo  "<h1>user age is $age </h1>";
+echo  "<h1>user salary is $salary </h1>";
+
+// echo "<pre>";
+// print_r($array);
+// echo "</pre>";
+
+echo $array[0] ."</br>";
+echo $array[1] ."</br>";
+echo $array[2] ."</br>";
+echo $array[3] ."</br>";
+echo $array[4];
+
+
+
+
+?>
