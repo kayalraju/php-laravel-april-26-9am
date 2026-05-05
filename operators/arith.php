2caf495703056fe47282af8cf65572a6321b6911
kayalraju <kayalraju003@gmail.com>
2026-05-05 09:33:14 +0530

    ope

---
+++ operators/arith.php
@@ -0,0 +1,16 @@
+
+
+<?php
+
+$num1=19;
+$num2=5;
+
+echo "sum of two number is = ". $num1+$num2 ."<br>";
+echo "subtraction of two number is = ".$num1-$num2 ."<br>";
+echo " multiplication of two number is = ".$num1*$num2 ."<br>";
+echo "division of two number is = ".$num1/$num2 ."<br>";
+echo "mod of two number is = ".$num1%$num2 ."<br>";
+
+
+
+?>
---
+++ operators/comparison.php
@@ -0,0 +1,19 @@
+<?php
+//equality  
+
+$num1=10;
+$num2=10;
+echo $num1==$num2 ."<br>";//true
+
+
+//comparison
+
+echo 5==5 ."<br>";//true
+echo 5==7 ."<br>";//false
+echo 5!=7 ."<br>";//true
+
+
+
+
+
+?>
