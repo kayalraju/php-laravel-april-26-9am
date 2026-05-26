971431068ba4ce3a8665c6a77f6f4b60c28f6289
kayalraju <kayalraju003@gmail.com>
2026-05-26 10:09:26 +0530

    include and require

---
+++ templeteCuting/common/navbar.php
@@ -0,0 +1,8 @@
+<nav class="container" style="background-color: #333; color: #fff; display: flex; justify-content: space-between;">
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        
+    </ul>
+</nav>
---
+++ templeteCuting/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ include 'common/navbar.php';
+//require 'common/navbard.php';
+?>
+
+<main class="con">
+    <h1>contact Page</h1>
+</main>
+
+
+    <?php include 'common/footer.php';  ?>
+</body>
+</html>
