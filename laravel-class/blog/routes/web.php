d1653341b88de37667018b6ff1ba6322406525bf
RajuKayal <learnbackend228@gmail.com>
2026-09-09 10:04:43 +0530

    laravel breeze

--- laravel-class/blog/routes/web.php
+++ laravel-class/blog/routes/web.php
@@ -1,14 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BladetempleteController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FilehandalingController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 
 Route::get('/about',[HomeController::class,'about']);
@@ -50,7 +64,7 @@ Route::post('/student/create',[FilehandalingController::class,'store'])->name('s
 
 //blade templete engine
 
-Route::get('/',[BladetempleteController::class,'index'])->name('home.view');
+Route::get('/home',[BladetempleteController::class,'index'])->name('home.view');
 Route::get('/about',[BladetempleteController::class,'about'])->name('about.view');
 Route::get('/contact',[BladetempleteController::class,'contact'])->name('contact.view');
 
@@ -58,11 +72,15 @@ Route::get('/contact',[BladetempleteController::class,'contact'])->name('contact
 Route::resource('/product',\App\Http\Controllers\ProductController::class);
 
 //card app
-Route::get('/student',[StudentController::class,'index'])->name('student.view');
-Route::get('/student/create',[StudentController::class,'create'])->name('student.create');
-Route::post('/student',[StudentController::class,'store'])->name('student.store');
-Route::get('/student/{id}',[StudentController::class,'edit'])->name('student.edit');
-Route::put('/student/{id}',[StudentController::class,'update'])->name('student.update');
-Route::get('/student/delete/{id}',[StudentController::class,'destroy'])->name('student.destroy');
+//middleware group
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/student',[StudentController::class,'index'])->name('student.view');
+    Route::get('/student/create',[StudentController::class,'create'])->name('student.create');
+    Route::post('/student',[StudentController::class,'store'])->name('student.store');
+    Route::get('/student/{id}',[StudentController::class,'edit'])->name('student.edit');
+    Route::put('/student/{id}',[StudentController::class,'update'])->name('student.update');
+    Route::get('/student/delete/{id}',[StudentController::class,'destroy'])->name('student.destroy');
 
-Route::get('/student/view/{id}',[StudentController::class,'view'])->name('student.single.view');
+    Route::get('/student/view/{id}',[StudentController::class,'view'])->name('student.single.view');
+    });
+
